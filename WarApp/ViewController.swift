2f203a5133d98ea16c011127328bf97ff4e8c8fd
Igor Bopp <igor_bopp@mail.ru>
2019-02-06 20:15:47 +0300

    Initial Commit

---
+++ WarApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarApp
+//
+//  Created by Игорь Бопп on 06/02/2019.
+//  Copyright © 2019 Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
